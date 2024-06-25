1739b463c3071a047785825da63da721e31009e3
Sumedha Singh Rathor <130545882+SumedhaSinghRathor@users.noreply.github.com>
2024-06-25 19:21:16 +0530

    Add files via upload

---
+++ scrolltext/scrolltext.js
@@ -0,0 +1,29 @@
+function marqueeAnimation() {
+    window.addEventListener("wheel", function(dets) {
+        if (dets.deltaY > 0) {
+            gsap.to("#marquee", {
+                transform: 'translateX(-200%)',
+                duration: 4,
+                repeat: -1,
+                ease: "none"
+            })
+    
+            gsap.to("#marquee img", {
+                rotate: 180
+            })
+        } else {
+            gsap.to("#marquee", {
+                transform: 'translateX(0%)',
+                repeat: -1,
+                duration: 4,
+                ease: "none"
+            })
+    
+            gsap.to("#marquee img", {
+                rotate: 0
+            })
+        }
+    })
+}
+
+marqueeAnimation()
